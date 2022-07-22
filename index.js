2e664746dd5655b5e4bf2d6e47a8fbcdb729cebe
Sumit Parise <82759795+SumitParise@users.noreply.github.com>
2022-07-23 01:32:24 +0530

    update js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var randomNumber1 = Math.round(Math.random()*5)+1;  // generate random number be
 
 var randomNumber2 = Math.round(Math.random()*5)+1;  // generate random number between 1 to 6
 
-var randomImages1 = "/images/dice"+ randomNumber1 +".png"; //Using random number to create random images with source of images
+var randomImages1 = "dice"+ randomNumber1 +".png"; //Using random number to create random images with source of images
 
-var randomImages2 = "/images/dice"+ randomNumber2 +".png"; //Using random number to create random images with source of images
+var randomImages2 = "dice"+ randomNumber2 +".png"; //Using random number to create random images with source of images
 
 var img1 = document.querySelectorAll("img")[0].setAttribute("src" , randomImages1);/*select the img tag to change attributes to*/ 
 
